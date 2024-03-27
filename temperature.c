129714ddc17169f4f63231c65684dd9fdcc7e3c3
stran rasol <stran@live.se>
2024-03-27 13:23:38 +0100

    changed the requested changes

--- temperature.c
+++ temperature.c
@@ -1,13 +1,15 @@
-const int motor = 7;
+#define blueLight 2
+#define yellowLight 3
+#define redLight 4
+#define motor 5
 const int sensorPin = A0;
 const float baseLineTemp = 20.0;
 
 void setup()
 {
     Serial.begin(9600);
-    pinMode(motor, OUTPUT);
 
-    for (int pinNum = 2; pinNum < 5; pinNum++)
+    for (int pinNum = 2; pinNum < 6; pinNum++)
     {
         pinMode(pinNum, OUTPUT);
         digitalWrite(pinNum, LOW);
@@ -17,38 +19,38 @@ void loop()
 {
     int sensorVal = analogRead(sensorPin);
 
-    Serial.print("Sensor Value: ");
+    // Serial.print("Sensor Value: ");
     Serial.print(sensorPin);
 
     float voltage = (sensorVal / 1024.0) * 5.0;
 
-    Serial.print(", Volts: ");
+    // Serial.print(", Volts: ");
     Serial.print(voltage);
 
-    Serial.print(", Degrees C: ");
-    float temp = (voltage - .5) * 100;
+    // Serial.print(", Degrees C: ");
+    float temp = (voltage - 0.5) * 100;
     Serial.print(temp);
 
     if (temp < baseLineTemp + 2)
     {
-        digitalWrite(2, HIGH);
-        digitalWrite(3, LOW);
-        digitalWrite(4, LOW);
-        digitalWrite(7, LOW);
+        digitalWrite(blueLight, HIGH);
+        digitalWrite(yellowLight, LOW);
+        digitalWrite(redLight, LOW);
+        digitalWrite(motor, LOW);
     }
     else if (temp >= baseLineTemp + 2 && temp < baseLineTemp + 4)
     {
-        digitalWrite(2, HIGH);
-        digitalWrite(3, HIGH);
-        digitalWrite(4, LOW);
-        digitalWrite(7, HIGH);
+        digitalWrite(blueLight, HIGH);
+        digitalWrite(yellowLight, HIGH);
+        digitalWrite(redLight, LOW);
+        digitalWrite(motor, HIGH);
     }
     else if (temp >= baseLineTemp + 6)
     {
-        digitalWrite(2, HIGH);
-        digitalWrite(3, HIGH);
-        digitalWrite(4, HIGH);
-        digitalWrite(7, HIGH);
+        digitalWrite(blueLight, HIGH);
+        digitalWrite(yellowLight, HIGH);
+        digitalWrite(redLight, HIGH);
+        digitalWrite(motor, HIGH);
     }
     delay(1);
 }
