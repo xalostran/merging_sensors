8498535bc36fc3a30d7faa32b7574493c7f22393
stran rasol <stran@live.se>
2024-03-29 00:00:06 +0100

    I will fix a new branch for the motor, approve this one

--- tempAndDisplay.c
+++ tempAndDisplay.c
@@ -7,9 +7,8 @@
 #define blueLight 7
 #define yellowLight 8
 #define redLight 9
-#define motor 10
 const int sensorPin = A0;
-const float baseLineTemp = 20.0;
+const float baseLineTemp = 17.0;
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -22,9 +21,6 @@ void setup()
     {
         pinMode(pinNum, OUTPUT);
         digitalWrite(pinNum, LOW);
-
-        // set up the LCD's number of columns and rows:
-        // Print a message to the LCD.
     }
     pinMode(sensorPin, INPUT);
     lcd.begin(16, 2);
@@ -32,22 +28,9 @@ void setup()
 
 void loop()
 {
-    // set the cursor to column 0, line 1
-    // (note: line 1 is the second row, since counting begins with 0):
-    // lcd.setCursor(0, 1);
-    // print the number of seconds since reset:
-    // lcd.print(millis() / 1000);
     int sensorVal = analogRead(sensorPin);
 
-    // Serial.print("Sensor Value: ");
     Serial.print(sensorPin);
-
-    // Serial.print(", Volts: ");
-    // Serial.print(voltage);
-
-    // Serial.print(", Degrees C: ");
-
-    // Serial.print(temp);
 
     if (sensorVal > 0 && sensorVal < 1024)
     {
@@ -59,7 +42,6 @@ void loop()
             digitalWrite(blueLight, HIGH);
             digitalWrite(yellowLight, LOW);
             digitalWrite(redLight, LOW);
-            // digitalWrite(motor, LOW);
             lcd.print("Degrees C:");
             lcd.print(temp);
             delay(2000);
@@ -68,10 +50,9 @@ void loop()
         }
         else if (temp >= baseLineTemp + 2 && temp < baseLineTemp + 4)
         {
-            digitalWrite(blueLight, HIGH);
+            digitalWrite(blueLight, LOW);
             digitalWrite(yellowLight, HIGH);
             digitalWrite(redLight, LOW);
-            // digitalWrite(motor, HIGH);
             lcd.print("Degrees C:");
             lcd.print(temp);
             delay(2000);
@@ -80,10 +61,9 @@ void loop()
         }
         else if (temp >= baseLineTemp + 6)
         {
-            digitalWrite(blueLight, HIGH);
-            digitalWrite(yellowLight, HIGH);
+            digitalWrite(blueLight, LOW);
+            digitalWrite(yellowLight, LOW);
             digitalWrite(redLight, HIGH);
-            // digitalWrite(motor, HIGH);
             lcd.print("Degress C:");
             lcd.print(temp);
             delay(2000);
@@ -96,4 +76,3 @@ void loop()
         }
         delay(1);
     }
-}
